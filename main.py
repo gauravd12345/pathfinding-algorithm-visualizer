5301235f66b99895c6a5841d80c6c5d5d858e93f
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2021-02-11 09:35:56 -0800

    added node functionality

--- main.py
+++ main.py
@@ -1,34 +1,86 @@
 import pygame
+from node import Node
 
 pygame.init()
 
-winSize = 700
-boxSize = winSize // 50
+winSize = 800
+boxNum = 50
+boxSize = winSize // boxNum
 
 win = pygame.display.set_mode((winSize, winSize))
 pygame.display.set_caption("Pathfinding Algorithm Visualizer")
 
 # Colors
-colors = {'w': (255, 255, 255),
-          'bl': (0, 0, 0),
-          'r': (255, 0, 0),
-          'g': (0, 255, 0),
-          'b': (0, 0, 255)}
+colors = {'white': (255, 255, 255),
+          'black': (0, 0, 0),
+         'silver': (128, 128, 128),
+            'red': (255, 0, 0),
+          'green': (0, 255, 0),
+           'blue': (0, 0, 255),
+      'lightblue': (66, 245, 239),
+    'lightorange': (252, 186, 3)}
 
 
-# Draws the grid onto the window
+# Initializes the blank grid of nodes
+nodeGrid = []
+for i in range(boxNum):
+    gridRow = []
+    for j in range(boxNum):
+        gridRow.append(Node(i, j, colors['white']))
+
+    nodeGrid.append(gridRow)
+
+
+# Clears all the nodes in the grid
+def clearNodeGrid(nodeGrid):
+    for gridRow in nodeGrid:
+        for node in gridRow:
+            if node.getColor() == colors['white'] or node.getColor() == colors['black']:
+                node.setColor(colors['white'])
+
+
+# Draws the grid of nodes onto the window
 def drawGrid():
-    rows, columns = 50, 50
-    for i in range(rows):
-        pygame.draw.line(win, colors['bl'], (i * boxSize, 0), (i * boxSize, winSize))
 
-    for j in range(columns):
-        pygame.draw.line(win, colors['bl'], (0, j * boxSize), (winSize, j * boxSize))
+    # Draws each node based on its position
+    for x in range(len(nodeGrid)):
+        for y in range(len(nodeGrid[x])):
+            row, col = nodeGrid[x][y].getPos()
+            color = nodeGrid[x][y].getColor()
+            pygame.draw.rect(win, color, (row * boxSize, col * boxSize, boxSize, boxSize))
+
+
+    # Draws the lines to form the grid        
+    for i in range(boxNum):
+        pygame.draw.line(win, colors['silver'], (i * boxSize, 0), (i * boxSize, winSize))
+
+    for j in range(boxNum):
+        pygame.draw.line(win, colors['silver'], (0, j * boxSize), (winSize, j * boxSize))
+
+
+
+# Returns the mouse position on the grid  
+def getMousePos():
+    x, y = pygame.mouse.get_pos()
+    return x // boxSize, y // boxSize
+
+
+# Returns the node directly under the mouse cursor
+def getNodeUnderMouse():
+    x, y = getMousePos()
+    return nodeGrid[x][y]
 
 
 clock = pygame.time.Clock()
-running = True
+running, selected = True, False
 
+startX, startY = 10, 10
+endX, endY = 40, 40
+startNode = nodeGrid[startX][startY]
+endNode = nodeGrid[endX][endY]
+
+startNode.setColor(colors['lightblue'])
+endNode.setColor(colors['lightorange'])
 
 # Gameloop
 while running:
@@ -36,11 +88,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-       
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            selected = True
 
-    win.fill(colors['w'])
+        if event.type == pygame.MOUSEBUTTONUP:
+            selected = False
+
+        # Clears the node grid
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                clearNodeGrid(nodeGrid)
+
+
+    # Changes the color of the node 
+    # if the mouse is clicked
+    if selected:
+        # Draws the node
+        if pygame.mouse.get_pressed()[0]:
+            node = getNodeUnderMouse()
+            if node.getColor() == colors['white']:
+                node.setColor(colors['black'])
+                
+        # Erases the node
+        elif pygame.mouse.get_pressed()[2]:
+            node = getNodeUnderMouse()
+            node.setColor(colors['white'])
+        
+
     drawGrid()
-
     pygame.display.update()
     clock.tick(60)
 
